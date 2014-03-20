f260784e004ea4e58810ddcae3a8cbf4796a7730
Morten E. Lund <mortenelund@gmail.com>
2014-03-20 14:31:36 +0100

    Completed Versjon 1
    Coding DONE!

--- TestGit.py
+++ TestGit.py
@@ -1,7 +1,8 @@
 # TestGIT.py in TestGIT2
 
 
-print('TestGIT2  *** Versjon 1.0')
-for i in range(10) :
+print('***** TestGIT2 *****')
+print('****** Ver 1.0 *****')
+for i in range(5) :
     print("TestGIT2")
         
